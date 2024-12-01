7136d615f3b2e22d33526633df0ef5928b4cb985
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-12-01 14:30:25 +0900

    Add files via upload

---
+++ bviste.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/vhrvnd13@main/4k.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
